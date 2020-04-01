0f2c95cbfa71f11a8f3c347eeac4ea25868d1a5a
vattcarter7 <vattcarter@gmail.com>
2020-04-01 22:34:33 +0700

    first commit

---
+++ client/src/firebase/key.sample.js
@@ -0,0 +1,12 @@
+// change this key values on your firebase console and rename this file to 'key.js'
+
+export default  {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
+};
---
+++ client/src/redux/shop/shop.actions.js
@@ -0,0 +1,33 @@
+import ShopActionTypes from './shop.types';
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionsMap
+});
+
+export const fetchCollectionsFailure = errorMessage => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage
+});
+
+
+// **** the code when implemeted rudux-thunk before redux-saga
+
+// export const fetchCollectionsStartAsync = () => {
+//   return dispatch => {
+//     const collectionRef = firestore.collection('collections');
+//     dispatch(fetchCollectionsStart());
+
+//     collectionRef
+//       .get()
+//       .then(snapshot => {
+//         const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+//         dispatch(fetchCollectionsSuccess(collectionsMap));
+//       })
+//       .catch(error => dispatch(fetchCollectionsFailure(error.message)));
+//   };
+// };
