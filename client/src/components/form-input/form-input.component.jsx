40d13bfb58fc59266214dfc2b331057c1bc50558
vattcarter7 <vattcarter@gmail.com>
2020-04-06 11:00:45 +0700

    fixed form input

--- client/src/components/form-input/form-input.component.jsx
+++ client/src/components/form-input/form-input.component.jsx
@@ -10,7 +10,7 @@ const FormInput = ({ handleChange, label, ...props }) => (
   <GroupContainer>
     <FormInputContainer onChange={handleChange} {...props} />
     {label ? (
-      <FormInputLabel>
+      <FormInputLabel className={props.value.length ? 'shrink' : ''}>
         {label}
       </FormInputLabel>
     ) : null}
